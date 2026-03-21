9ba0c5ea277e6ed3462243ab339be581250cd779
Patrick Collins <pscollins@google.com>
2026-03-21 11:46:31 -0400

    tweak script logging

--- mpl/bench/quickhull-minimize/test.sh
+++ mpl/bench/quickhull-minimize/test.sh
@@ -1,6 +1,5 @@
-set -x
 cd $(dirname $0)
 TEMPFILE=$(mktemp)
 make clean
 make bin/quickhull.bin 2>&1 | tee $TEMPFILE
-grep "Found forbidden tuple operations" $TEMPFILE
+grep -q "Found forbidden tuple operations" $TEMPFILE
