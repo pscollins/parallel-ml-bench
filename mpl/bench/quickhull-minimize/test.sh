25e203062936b5d07bb68b109ea95143aa51d6e1
Patrick Collins <pscollins@google.com>
2026-03-21 11:50:51 -0400

    tweak script printout

--- mpl/bench/quickhull-minimize/test.sh
+++ mpl/bench/quickhull-minimize/test.sh
@@ -2,4 +2,10 @@ cd $(dirname $0)
 TEMPFILE=$(mktemp)
 make clean
 make bin/quickhull.bin 2>&1 | tee $TEMPFILE
-grep -q "Found forbidden tuple operations" $TEMPFILE
+if grep -q "Found forbidden tuple operations" $TEMPFILE; then
+    echo "Test PASS -- bug still present!"
+    exit 0
+else
+    echo "Test FAIL! BUG NO LONGER REPRODUCES!"
+    exit 1
+fi
