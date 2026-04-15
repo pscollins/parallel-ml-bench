b833e59b85251044af2bf9180cf9717b7795bc2e
Patrick Collins <pscollins@google.com>
2026-04-15 11:23:51 -0400

    add profiling flow

---
+++ mpl/run_profile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Call as: ./run_profile $MPL_INVOCATION
+#
+# then run:
+#
+#  ~/go/bin/pprof -http=localhost:8080 bin/output.prof
+export CPUPROFILE=bin/output.prof
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libprofiler.so
+
